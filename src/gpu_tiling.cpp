efebd5782ea4ca401c26455182ff1efe3f91f6f3
onlyxItachi <hamzausta2222@gmail.com>
2026-03-29 18:12:23 +0300

    Revert "Agent 2: Repair SCF accumulator loop construction"
    This reverts commit 93540000c5f0399ca6bd0cf0e99eab5df26165c7.

--- src/gpu_tiling.cpp
+++ src/gpu_tiling.cpp
@@ -666,7 +666,8 @@ struct PersistNvidiaMmaAccumulatorPass
 
       mlir::Block *new_body = new_loop.getBody();
       mlir::Value loop_carried_accumulator = new_loop.getRegionIterArgs().front();
-      builder.setInsertionPointToStart(new_body);
+      auto yield = llvm::cast<mlir::scf::YieldOp>(new_body->getTerminator());
+      builder.setInsertionPoint(yield);
       for (mlir::Operation &op : body->without_terminator()) {
         if (&op == mma_sync_op.getOperation() ||
             accumulator_load_slice.contains(&op) ||
@@ -679,7 +680,7 @@ struct PersistNvidiaMmaAccumulatorPass
       mlir::Operation *cloned_mma =
           builder.clone(*mma_sync_op.getOperation(), mapping);
       cloned_mma->setOperand(2, loop_carried_accumulator);
-      builder.create<mlir::scf::YieldOp>(loop.getLoc(), cloned_mma->getResult(0));
+      yield->setOperands(cloned_mma->getResult(0));
 
       builder.setInsertionPointAfter(new_loop);
       storeAccumulatorFragmentToWorkgroup(builder, loop.getLoc(),
