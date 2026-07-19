ed7f19705761aaa87dbcbdcc7dfa06c1dcb8d34a
onlyxItachi <hamzausta2222@gmail.com>
2026-07-19 16:52:25 +0300

    build(mdslc): add isolated standalone CMake skeleton

---
+++ compiler/tools/mdslc/main.cpp
@@ -0,0 +1,9 @@
+#include "mdslc_config.h"
+
+#include <iostream>
+
+int main() {
+  std::cout << "mdslc++ bootstrap driver (clang++: "
+            << MDSLC_DEFAULT_CLANGXX << ")\n";
+  return 0;
+}
